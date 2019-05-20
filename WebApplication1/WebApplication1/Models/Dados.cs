bfff03b708baca57f320d6e742049201399f3620
tmilitino <tmilitino.tm@gmail.com>
2019-05-20 13:55:04 -0300

    inclusão de mais classe ao model

--- WebApplication1/WebApplication1/Models/Dados.cs
+++ WebApplication1/WebApplication1/Models/Dados.cs
@@ -21,10 +21,10 @@ namespace WebApplication1.Models
 
         }
 
-        public Entrada(Produto produto, Cliente cliente, char ent_status, DateTime ent_dt_cad, decimal ent_valor, decimal ent_desconto, char ent_formaPag)
+        public Entrada(Produto produto, Cliente cliente, bool ent_status, DateTime ent_dt_cad, decimal ent_valor, decimal ent_desconto, char ent_formaPag)
         {
-            this.produto = produto;
-            this.cliente = cliente;
+            this.Produto = produto;
+            this.Cliente = cliente;
             Ent_status = ent_status;
             Ent_dt_cad = ent_dt_cad;
             Ent_valor = ent_valor;
@@ -33,14 +33,14 @@ namespace WebApplication1.Models
         }
 
         [Required]
-       public Produto produto { get; private set; }
+       public Produto Produto { get; private set; }
 
 
         [Required]
-        public Cliente cliente { get; private set; }
+        public Cliente Cliente { get; private set; }
 
         [Required]
-        public char Ent_status { get; private set; }
+        public bool Ent_status { get; private set; }
 
         [DataType(dataType: DataType.DateTime)]
         public DateTime Ent_dt_cad { get; private set; }
@@ -48,14 +48,31 @@ namespace WebApplication1.Models
         [Required]
         public decimal Ent_valor { get; private set; }
         
-    [Required]
-    public decimal Ent_desconto { get; private set; }
+        [Required]
+        public decimal Ent_desconto { get; private set; }
 
         [Required]
         public char Ent_formaPag { get; private set; }
     }
 
-    public class Cliente:BaseDados
+    public class Apresentacao : BaseDados
+    {
+        public Apresentacao()
+        {
+        }
+
+        public Apresentacao(int ape_id, string ape_Nome, char ape_sigla)
+        {
+            Ape_id = ape_id;
+            Ape_Nome = ape_Nome;
+            Ape_sigla = ape_sigla;
+        }
+
+        public int Ape_id { get; private set; }
+        public string Ape_Nome { get; private set; }
+        public char Ape_sigla { get; private set; }
+    }
+        public class Cliente:BaseDados
     {
         public Cliente()
         {
@@ -72,5 +89,77 @@ namespace WebApplication1.Models
 
         }
 
+        public Produto(Receita receita, int prod_id, string prod_nome, decimal prod_preco, bool prod_status, DateTime prod_dt_cad)
+        {
+            this.Receita = receita;
+            Prod_id = prod_id;
+            Prod_nome = prod_nome;
+            Prod_preco = prod_preco;
+            Prod_status = prod_status;
+            Prod_dt_cad = prod_dt_cad;
+        }
+
+        [Required]
+        public Receita Receita { get; private set; }
+
+        [Required]
+        public int Prod_id { get; private set; }
+
+        [Required]
+        public string Prod_nome { get; private set; }
+
+        [Required]
+        public decimal Prod_preco { get; private set; }
+
+        [Required]
+        public bool Prod_status { get; private set; }
+
+        [Required]
+        [DataType(dataType: DataType.DateTime)]
+        public DateTime Prod_dt_cad { get; private set; }
+
+    }
+
+
+    public class Receita
+    {
+        public Receita()
+        {
+        }
+
+        public Receita(int rec_id, Item item, decimal rec_item_qtd, bool rec_status, DateTime rec_dt_cad)
+        {
+            Rec_id = rec_id;
+            Item = item;
+            Rec_item_qtd = rec_item_qtd;
+            Rec_status = rec_status;
+            Rec_dt_cad = rec_dt_cad;
+        }
+
+        [Required]
+        public int Rec_id { get; protected set; }
+
+        [Required]
+        public Item Item { get; protected set; }
+
+        [Required]
+        public decimal Rec_item_qtd { get; protected set; }
+
+        [Required]
+        public bool Rec_status { get; protected set; }
+
+        [Required]
+        [DataType(dataType: DataType.DateTime)]
+        public DateTime Rec_dt_cad { get; private set; }
+
+
+
+    }
+
+    public class Item
+    {
+        public Item()
+        {
+        }
     }
 }
